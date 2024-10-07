5e7b32472066c7f46b707a2338bc11d107d29810
Ronan Kelly <Kelly-R95@ulster.ac.uk>
2024-10-07 16:18:48 +0100

    Created a calculator

--- Week 1 - Calculator/Program.cs
+++ Week 1 - Calculator/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Console.WriteLine("Type in the first number, and then press enter");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("type the second number, and then press enter");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
